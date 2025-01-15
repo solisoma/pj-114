e1f370af76fb12f2a8ad200d6f660cdf4d00cd1d
solisoma <solisomamadubueze@gmail.com>
2025-01-15 19:01:58 +0100

    completed profile

--- frontend/components/dashboard/Scaffold.tsx
+++ frontend/components/dashboard/Scaffold.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { sidenavs } from "@/utils/scaffold";
 import AuthGuard from "../auth/AuthGuard";
 import { clearSecureStorage, hash_notis, is_same } from "@/api/auth";
@@ -28,6 +28,7 @@ function Scaffold({ children, activeLink, route }: ScaffoldType) {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [refresh, setRefresh] = useState<boolean>(false);
   const router = useRouter();
+  const elementRef = useRef(null);
   const Children = React.cloneElement(children, {
     userId: userDetail.id,
     permission: userDetail.permission,
@@ -47,6 +48,12 @@ function Scaffold({ children, activeLink, route }: ScaffoldType) {
     }
   }
 
+  const updateHeight = () => {
+    if (elementRef.current) {
+      (elementRef.current as any).style.height = `${window.innerHeight}px`;
+    }
+  };
+
   async function setUser() {
     const get_user = await get_user_status();
     setUserDetail(get_user);
@@ -55,12 +62,18 @@ function Scaffold({ children, activeLink, route }: ScaffoldType) {
 
   useEffect(() => {
     // setUser();
+    updateHeight(); // Set height on load
+    window.addEventListener("resize", updateHeight); // Adjust height on resize
+
+    return () => {
+      window.removeEventListener("resize", updateHeight);
+    };
   }, [refresh]);
 
   return (
     !loading && (
       <>
-        <div className="flex h-screen w-screen">
+        <div className="flex w-screen">
           <div
             onClick={(e: any) =>
               e.target.id === "parent" && setShowMobileMenu(false)
@@ -68,7 +81,7 @@ function Scaffold({ children, activeLink, route }: ScaffoldType) {
             id="parent"
             className={`absolute top-0 z-[999] h-full w-full bg-transparent ${
               showMobileMenu ? "translate-x-0" : "-translate-x-full"
-            } transition-transform duration-700 md:w-[20%] md:translate-x-0 md:static border`}
+            } transition-transform duration-700 md:w-[20%] md:translate-x-0 md:static`}
           >
             <div className="bg-background h-full w-[65%] md:w-full px-[1rem] md:px-[1.4vw] border-r">
               <div className="h-[15%] md:py-[.9vw]">
@@ -95,7 +108,7 @@ function Scaffold({ children, activeLink, route }: ScaffoldType) {
               </div>
             </div>
           </div>
-          <div className="w-full bg-background3 md:w-[95%] h-full overflow-y-scroll remove-scrollbar">
+          <div className="w-full bg-background3 md:w-[95%] h-full overflow-y-scroll remove-scrollbar border">
             <div className="fixed z-[99] top-0 w-full md:w-[82%] flex justify-between items-center bg-background h-[10%] p-6">
               <div className="flex items-center gap-3 md:hidden">
                 <Link href="/">
